26a06a4833d347dc2b93de20e1fc9ed90453264a
sanderdebr <sander606@gmail.com>
2019-12-25 06:48:43 +0100

    update

---
+++ public/chat.js
@@ -0,0 +1,38 @@
+$(function(){
+    // make connection
+    var socket = io.connect('http://localhost:3000')
+
+    //buttons and input
+    var message = $("#message");
+    var username = $("#username");
+    var send_message = $("#send_message");
+    var send_username = $("#send_username");
+    var chatroom = $("#chatroom");
+    var feedback = $("#feedback")
+
+    // Emit a username
+    send_username.click(function() {
+        socket.emit('change_username', {username: username.val()})
+    })
+
+    // Emit a message
+    send_message.click(function() {
+        socket.emit('new_message', {message: message.val()})
+    })
+
+    // listen on new message
+    socket.on("new_message", (data) => {
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    //Emit typing
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	})
+
+	//Listen on typing
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	})
+
+});
